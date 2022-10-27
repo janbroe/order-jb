713c612d48486b7d738e6b9d5731188a61197177
Jan Broeckx <jan.broeckx@cegeka.com>
2022-10-27 11:45:38 +0200

    initial commit

---
+++ src/main/java/com/switchfully/war/OrderJbApplication.java
@@ -0,0 +1,13 @@
+package com.switchfully.war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderJbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderJbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/switchfully/war/OrderJbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchfully.war;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderJbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
