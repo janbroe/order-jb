bd813adfd477413de9c932b2f496dcf8bb608422
Jan Broeckx <jan.broeckx@cegeka.com>
2022-11-03 09:00:36 +0100

    added integration test for story 4, updating an item

--- src/test/java/com/switchfully/ItemIntegrationTest.java
+++ src/test/java/com/switchfully/ItemIntegrationTest.java
@@ -17,6 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import java.util.Base64;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -71,6 +72,108 @@ public class ItemIntegrationTest {
         assertThat(result.getDescription()).isEqualTo(given.getDescription());
         assertThat(result.getPrice()).isEqualTo(given.getPrice());
         assertThat(result.getAmount()).isEqualTo(given.getAmount());
+
+    }
+
+    @Test
+    void updateItemHappyPath() {
+
+        String authorization = Base64.getEncoder().encodeToString("jth@hotmail.com:pwd".getBytes());
+        userRepository.createUser(new User().setFirstname("Jaak").setLastname("Trekhaak").setEmail("jth@hotmail.com").setAddress("Remorkbaan 66").setPhoneNumber("04999001122").setPassword("pwd").setRole(Role.ADMIN));
+
+        Item initialItem = itemRepository.getAllItems().stream()
+                .filter(items -> items.getName().equals("firework"))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("firework item not found in item repo"));
+
+        CreateItemDTO given = new CreateItemDTO()
+                .setName("Big firework")
+                .setDescription("nice exploding stuff with extra boom boom")
+                .setPrice(68.99)
+                .setAmount(10);
+
+        RestAssured
+                .given()
+                .body(given)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .baseUri("http://localhost")
+                .headers("Authorization", "Basic " + authorization)
+                .port(port)
+                .when()
+                .put("items?itemId=" + initialItem.getId())
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.CREATED.value());
+
+        Item result = itemRepository.getItemById(initialItem.getId());
+
+        assertThat(result).isNotNull();
+        assertThat(result.getName()).isEqualTo(given.getName());
+        assertThat(result.getDescription()).isEqualTo(given.getDescription());
+        assertThat(result.getPrice()).isEqualTo(given.getPrice());
+        assertThat(result.getAmount()).isEqualTo(given.getAmount());
+
+    }
+
+    @Test
+    void whenUpdateItemWithWrongCredentials_thenExpectAuthorizationException() {
+
+        String authorization = Base64.getEncoder().encodeToString("notAdmin@hotmail.com:pwd".getBytes());
+        userRepository.createUser(new User().setFirstname("Jos").setLastname("Vos").setEmail("notAdmin@hotmail.com").setAddress("vosbaan 3").setPhoneNumber("04999334455").setPassword("pwd").setRole(Role.CUSTOMER));
+
+        Item initialItem = itemRepository.getAllItems().stream()
+                .filter(items -> items.getName().equals("firework"))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("firework item not found in item repo"));
+
+        CreateItemDTO given = new CreateItemDTO()
+                .setName("Big firework")
+                .setDescription("nice exploding stuff with extra boom boom")
+                .setPrice(68.99)
+                .setAmount(10);
+
+        RestAssured
+                .given()
+                .body(given)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .baseUri("http://localhost")
+                .headers("Authorization", "Basic " + authorization)
+                .port(port)
+                .when()
+                .put("items?itemId=" + initialItem.getId())
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.UNAUTHORIZED.value());
+
+    }
+
+    @Test
+    void whenUpdateItemWithWrongItemId_thenExpectNoSushElementException() {
+
+        String authorization = Base64.getEncoder().encodeToString("jth@hotmail.com:pwd".getBytes());
+        userRepository.createUser(new User().setFirstname("Jaak").setLastname("Trekhaak").setEmail("jth@hotmail.com").setAddress("Remorkbaan 66").setPhoneNumber("04999001122").setPassword("pwd").setRole(Role.ADMIN));
+
+        CreateItemDTO given = new CreateItemDTO()
+                .setName("Big firework")
+                .setDescription("nice exploding stuff with extra boom boom")
+                .setPrice(68.99)
+                .setAmount(10);
+
+        RestAssured
+                .given()
+                .body(given)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .baseUri("http://localhost")
+                .headers("Authorization", "Basic " + authorization)
+                .port(port)
+                .when()
+                .put("items?itemId=" + "wrongItemId")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
 
     }
 }
